3c91fe60b96680957165a701149f9ec7e563557a
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-06-29 20:58:38 +0530

    Initial backend setup

---
+++ SocietyMaintenance-Backend/src/test/java/com/kavya/societymaintenance/SocietyMaintenance/SocietyMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavya.societymaintenance.SocietyMaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
